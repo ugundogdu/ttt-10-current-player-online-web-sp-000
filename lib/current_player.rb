c5842589e26278b1297dfae0302bcc015cf80809
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-09 19:26:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,23 @@
 def turn_count(board)
+  # kac kez oynandigini tespit etmek
+
   move_counter = 0
-  board.each { |item| move_counter += 1 unless item == ' ' }
+
+  board.each do |item|
+    if item == 'X' || item == 'O'
+      move_counter += 1
+    end
+  end
+
   move_counter
 end
 
 def current_player(board)
-  turn_count(board).even? ? 'X' : 'O'
+  # 1-3-5-7 O
+  # 0-2-4-6-8 X
+  if turn_count(board) % 2 == 0
+    return 'X'
+  else
+    return 'O'
+  end
 end

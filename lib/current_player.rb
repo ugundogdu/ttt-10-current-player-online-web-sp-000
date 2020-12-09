1e54ae4e6b5b9fd745ae205e56896b2542b5d03f
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-09 18:51:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,7 @@
 def turn_count(board)
   # kac kez oynandigini tespit etmek
+
   move_counter = 0
-
   board.each do |item|
-    if item == 'X' || item == 'O'
-      move_counter += 1
-    end
-  end
-
-  move_counter
+    if == 'X' ||
 end
